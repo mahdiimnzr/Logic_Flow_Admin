a309dc6d9fc2af634d5a92318bf6dae89d0f8e3b
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-07-25 23:13:04 +0330

    dev: completing two languages

--- src/components/blogs/details/BlogContentDisplay.js
+++ src/components/blogs/details/BlogContentDisplay.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Card, CardHeader, CardTitle, CardBody } from 'reactstrap'
-import HandleIdentityEditorJs from '../../common/EditorDetailValidation'
+import React from "react";
+import { Card, CardHeader, CardTitle, CardBody } from "reactstrap";
+import HandleIdentityEditorJs from "../../common/EditorDetailValidation";
+import { useTranslation } from "react-i18next";
 
 const BlogContentDisplay = ({ content }) => {
-    return (
-        <Card>
-            <CardHeader className="border-bottom mb-1">
-                <CardTitle tag="h4">محتوای مقاله</CardTitle>
-            </CardHeader>
-            <CardBody>
-                {content ? (
-                    <HandleIdentityEditorJs desc={content} />
-                ) : (
-                    <div className="text-center text-muted py-5">
-                        محتوایی برای این مقاله ثبت نشده است.
-                    </div>
-                )}
-            </CardBody>
-        </Card>
-    )
-}
+  const { t } = useTranslation();
+  return (
+    <Card>
+      <CardHeader className="border-bottom mb-1">
+        <CardTitle tag="h4">{t("BlogContent")}</CardTitle>
+      </CardHeader>
+      <CardBody>
+        {content ? (
+          <HandleIdentityEditorJs desc={content} />
+        ) : (
+          <div className="text-center text-muted py-5">
+            {t("BlogNoContent")}
+          </div>
+        )}
+      </CardBody>
+    </Card>
+  );
+};
 
-export default BlogContentDisplay
+export default BlogContentDisplay;
